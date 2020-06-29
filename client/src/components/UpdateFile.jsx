54fc30fb78311bd81aad258b326d3bd5f5ce3886
rolan19 <rolando19jobs@gmail.com>
2020-06-29 18:10:54 -0500

    cambios de i

--- client/src/components/UpdateFile.jsx
+++ client/src/components/UpdateFile.jsx
@@ -1,20 +1,11 @@
 import React from "react";
 import "../styles/UpdateFile.css";
-const UpdateFile = ({ children, handleSubmit, caption, handleChange }) => {
+const UpdateFile = ({ children, handleSubmit }) => {
   return (
     <section>
-      <h1>UpdateFile</h1>
       <div className="Upload">
         <form onSubmit={handleSubmit}>
           <div className="Upload__image">{children}</div>
-          <textarea
-            name="caption"
-            className="Upload__caption"
-            required
-            maxLength="180"
-            value={caption}
-            onChange={handleChange}
-          ></textarea>
           <button className="Upload__submit" type="submit">
             Publicar
           </button>
